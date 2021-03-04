86e1e99a7727103dc61b33eda0df3e052e81bfaf
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-03-04 10:07:41 -0500

    merge repositories

--- source/elements/grid.js
+++ source/elements/grid.js
@@ -1,9 +1,5 @@
-import html from '../templates/grid.js';
-
 export class Grid extends Quantum {
     constructor() {
         super();
     }
 }
-
-Grid.define('quantum-grid', html);
--- source/main.js
+++ source/main.js
@@ -1,1 +1,4 @@
-import './elements/grid.js';
+import { Grid } from './elements/grid.js';
+import grid from './templates/grid.js';
+
+Grid.define('quantum-grid', grid);
