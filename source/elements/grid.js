c4e8e7265778c8fc283d3eaaa13d6b0550dc9d3b
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:47:10 -0400

    update build

--- source/elements/grid.js
+++ source/elements/grid.js
@@ -1,5 +1,9 @@
+import grid from '../templates/grid.js';
+
 export class Grid extends Quantum {
     constructor() {
         super();
     }
 }
+
+Grid.define('quantum-grid', grid);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Grid } from './elements/grid.js';
-import grid from './templates/grid.js';
-
-Grid.define('quantum-grid', grid);
