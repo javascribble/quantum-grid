eadba28c3d175a203c7a1bbfd5c116b5afa0cc36
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-10-14 08:15:52 -0400

    add default style

--- source/templates/grid.js
+++ source/templates/grid.js
@@ -1,5 +1,24 @@
 export default `
 <style>
+    table {
+        border-collapse: collapse;
+    }
 
+        table thead th {
+            text-align: left;
+        }
+
+        table tbody tr:nth-child(2n + 1) {
+            background-color: var(--secondary-background-color);
+        }
+
+        table tbody td {
+            padding: var(--space-small);
+            border: solid var(--border-size) var(--border-color);
+        }
+
+        table tfoot td {
+            border: 0;
+        }
 </style>
 `;
